0da09575bbbca160c2f317edeec56a7cb4e85315
t0osh <jeandavid.jdh@gmail.com>
2015-03-23 15:10:44 +0000

    Tweak du dev-server

--- dev-server.js
+++ dev-server.js
@@ -2,13 +2,20 @@ var WebpackDevServer = require('webpack-dev-server'),
     webpack = require('webpack'),
     config = require('./webpack.local.config');
 
-new WebpackDevServer(webpack(config), {
+var app = new WebpackDevServer(webpack(config), {
   hot: true,
   inline: true,
   quiet: true,
   contentBase: {
     target: 'http://localhost:' + (process.env.SERVER_PORT || 8080)
   }
-}).listen(process.env.DEV_PORT || 8081, function(err, result) {
+});
+
+var server = app.listen(process.env.DEV_PORT || 8081, function(err, result) {
   if(err) throw err;
+
+  var host = "localhost";
+  var port = process.env.DEV_PORT || 8081;
+  
+  console.log("Dev server listening at http://%s:%s", host, port);
 });
